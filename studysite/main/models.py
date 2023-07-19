6f3d0a2b675ff8c9ad25ee89352223170928ab32
appypaw <siment3187@gmail.com>
2023-07-19 15:50:16 +0900

    DB & Admin Panel

--- studysite/main/models.py
+++ studysite/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host = 
+    #topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)        #CharField와 TextField는 비슷한데 TextField는 조금 더 Large함. 텍스트(김)와 단순 문자열의 차이. 이름은 기껏해야 10글자 내외인데 설명은 김. 
+    #null=True는 DB 수준에서 NULL값을 허용한다는 의미. blank=True는 폼 레벨에서 필수가 아니라는 이야기=비워도 유효성 검사 통과.
+    #participants = 
+    updated = models.DateTimeField(auto_now=True)
--- studysite/main/views.py
+++ studysite/main/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
+from .models import Room
+
+"""
 rooms = [
     {'id':1, 'name':'파이썬을 배워봅시다.'},
     {'id':2, 'name':'디자인도 해봅시다..'},
     {'id':3, 'name':'프론트엔드 개발자.'},
     {'id':4, 'name':'백엔드 개발자.'},
 ]
+"""
 
 def home(request):
+    rooms = Room.objects.all()    #model 매니저
     context = {'rooms': rooms}
     return render(request, 'main/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
 
     return render(request, 'main/room.html', context)
