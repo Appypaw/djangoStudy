c8948de9d516cb3779235d7f00878dcc79443975
appypaw <siment3187@gmail.com>
2023-07-23 21:59:31 +0900

    models 공부

--- studysite/main/models.py
+++ studysite/main/models.py
@@ -10,3 +10,12 @@ class Room(models.Model):
     #null=True는 DB 수준에서 NULL값을 허용한다는 의미. blank=True는 폼 레벨에서 필수가 아니라는 이야기=비워도 유효성 검사 통과.
     #participants = 
     updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)            
+    #auto_now와 auto_now_add의 차이점. auto_now는 스냅샷을 찍었을 때마다, auto_now _add는 업데이트 할 때만.
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    #user = 
+    room = models.ForeignKey(Room)
